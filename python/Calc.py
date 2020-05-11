dc66150bc1b51081de78af2bf81148a800cf67af
moxd001 <you@example.com>
2020-05-11 14:15:00 +0800

    commit

---
+++ python/Calc.py
@@ -0,0 +1,11 @@
+class Calc:
+    def add(self, a:int, b:int):
+        return a + b
+
+    def div(self, a:int, b:int):
+        return a / b
+    def sub(self,a:int,b:int):
+        return a-b
+    def mul(self,a:int,b:int):
+        return a*b
+
